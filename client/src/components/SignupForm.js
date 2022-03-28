f296c4f267033dbbab918b8b6431b48430a55fa9
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-03-27 20:10:58 -0600

    replaced addUser function with ADD_USER mutation

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+//added Mutation
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 const SignupForm = () => {
   // set initial form state
@@ -11,6 +12,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, {error, data}] = useMutation(ADD_USER); 
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,7 +31,9 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const {response} = await addUser({
+        variables: {...userFormData}
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
@@ -37,6 +42,7 @@ const SignupForm = () => {
       const { token, user } = await response.json();
       console.log(user);
       Auth.login(token);
+
     } catch (err) {
       console.error(err);
       setShowAlert(true);
