fba67d72525a8300399e716158634842596d9254
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-03-27 20:22:31 -0600

    added app.get to server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -24,6 +25,10 @@ if (process.env.NODE_ENV === 'production') {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
